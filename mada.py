02184a141765c1e800126333a0894e96427146ec
madalinagrama <m.madalinagrama@gmail.com>
2020-11-03 01:04:40 +0200

    [shoot_at_coordinates] defining function

--- mada.py
+++ mada.py
@@ -1,14 +1,32 @@
-def shoot_at_coordinates(game_map, x_axis, y_axis):
-    if game_map[y_axis][x_axis] == representation_water_on_map:
-        game_map[y_axis][x_axis] = representation_miss_on_map
-        print("YOU HAVE MISSED.")
+ships_for_player1 = [2,1]
+ships_for_player2 = [2,1]
+representation_water_on_map = 0 
+representation_miss_on_map = 3
+representation_ship_on_map = 1
+representation_hit_ship_on_map = 2
+
+
+def shoot_at_coordinates(game_map, x_axis, y_axis):  
+    if game_map[x_axis][y_axis] == representation_water_on_map:
+        game_map[x_axis][y_axis] == representation_miss_on_map
+        print("You have missed")
         return
-    if game_map[y_axis][x_axis] == representation_intact_ship_on_map:
+    if game_map[x_axis][y_axis] == representation_ship_on_map:
         if ship_has_no_more_lives(game_map, x_axis, y_axis):
             mark_whole_ship_as_dead(game_map, x_axis, y_axis)
-            print("YOU HAVE SUNK A SHIP")
+            print("You have sunk a ship!")
         else:
-            game_map[y_axis][x_axis] = representation_hit_ship_on_map
-            print("YOU HAVE HIT A SHIP")
+            game_map[x_axis][y_axis] == representation_hit_ship_on_map
+            print('You have hit a ship!')
         return
-    print("YOU HAVE HIT A PLACE THAT WAS ALREADY HIT")
+    print('You have made a hit at previous hit place!')
+game_map = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+
+
