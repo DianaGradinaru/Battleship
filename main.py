778fb920f0f13abdd1b0bb7c221ef97fd19ae3b9
github MadalinaMihaela89 <github madalina.jerca@gmail.com>
2020-11-04 13:44:18 +0200

    [madi_fun] Update

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ representation_intact_ship_on_map = 1
 representation_hit_ship_on_map = 2
 
 
+#   Ii dau functiei DISPLAY_CURRENT_PLAYER doi parametrii: care e jucatorul care trage acum -> acesta
+# poate fi MAP1 sau MAP2. Desi incepe cu MAP1, se poate schimba in urmatoarea iteratie si poate sa devina
+# MAP2 (din temp = ... , vezi liniile 64 - 66) unde se face swich maps. Daca variabila care se afla in
+# SHOOTING_PLAYER_MAP este tot MAP1, atunci pot sa afisez din functia DISPLAY_CURRENT_PLAYER -> player 1
+# is shooting! Daca atat in CURRENT_PLAYER_MAP cat si in PLAYER_ONE_MAP se afla MAP1 (vezi linia 52 !!!),
+# atunci este harta jucatorului 1. Dar daca in CURRENT_PLAYER_MAP se afla harta celui de-al doilea
+# jucator, asta inseamna ca nu e MAP1.
+
+
 def display_current_player_turn(current_player_map, player_one_map):
+    # daca in ambele "galeti" am aceiasi harta, atunci e randul jucatorului 1
     if current_player_map == player_one_map:
         print("=== PLAYER 1 IS SHOOTING NOW ===")
     else:
@@ -28,20 +38,29 @@ pass
 
 def start():
     print("=== PLAYER 1 TURN TO PLACE SHIPS ON MAP ===")
+    # aici avem harta cu navele primului jucator
     map1 = place_ships_on_map(ships_for_player1)
     print("=== PLAYER 2 TURN TO PLACE SHIPS ON MAP ===")
+    # aici avem harta cu navele celui de-al doilea jucator
     map2 = place_ships_on_map(ships_for_player2)
 
+    # pentru a sti care e primul jucator care trage, spunem ca e randul celui care are map1
     shooting_player_map = map1
     enemy_map = map2
-    while True:
+    while True:  # iteram fiecare tura in care trage un jucator
+        # spunem randul carui jucator este
         display_current_player_turn(shooting_player_map, map1)
+        # citim coordonatele unde vrem sa tragem
         [y_axis, x_axis] = read_coordinates()
+        # vrem sa tragem la aceste coordonate in harta inamicului
         shoot_at_coordinates(enemy_map, x_axis, y_axis)
+        # dupa ce am tras, aratam harta inamicului
         display_enemy_map(enemy_map)
+        # vrem sa vedem daca inamicul mai are nave intacte, daca nu -> display winner
         if has_lost(enemy_map):
             display_winner(shooting_player_map, map1)
             return
+        # daca nu a castigat jucatorul curent, ii inversam prin metoda variabilei intermediare
         temp = shooting_player_map
         shooting_player_map = enemy_map
         enemy_map = temp
